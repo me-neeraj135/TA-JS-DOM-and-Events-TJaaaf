361df88c73788069e6672fe12bb5d822ce771a21
me-neeraj135 <me.neeraj1455@gmail.com>
2022-02-14 12:31:27 +0530

    ramdom-color-generetor

---
+++ block-BJaaek/code/script.js
@@ -0,0 +1,8 @@
+let first = document.querySelector(`.first`);
+let btn = document.querySelector(`.banner`);
+
+btn.onclick = function () {
+  console.log(`you clicked me!`);
+};
+
+first.addEventListener(`click`, function () {});
