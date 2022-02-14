1cd90dab418388d309ff2ffe4169e47639db388e
me-neeraj135 <me.neeraj1455@gmail.com>
2022-02-14 14:43:11 +0530

    event-ramdom-color-generator

--- block-BJaaek/code/script.js
+++ block-BJaaek/code/script.js
@@ -1,8 +1,49 @@
-let first = document.querySelector(`.first`);
-let btn = document.querySelector(`.banner`);
+let one = document.querySelector(`.first`);
+let two = document.querySelector(`.second`);
 
-btn.onclick = function () {
-  console.log(`you clicked me!`);
-};
+function generateRandomColor() {
+  let hexValue = [
+    `0`,
+    `1`,
+    `2`,
+    `3`,
+    `4`,
+    `5`,
+    `6`,
+    `7`,
+    `8`,
+    `9`,
+    `a`,
+    `b`,
+    `c`,
+    `d`,
+    `e`,
+    `f`,
+  ];
+  let color = "#";
 
-first.addEventListener(`click`, function () {});
+  for (let i = 0; i < 6; i++) {
+    let randomNum = Math.floor(Math.random() * 16);
+    color = color + hexValue[randomNum];
+  }
+
+  return color;
+}
+
+// console.log(generateRandomColor());
+
+function handleClick() {
+  let randomColor = generateRandomColor();
+
+  one.style.backgroundColor = randomColor;
+}
+
+function onMousemove() {
+  let randomColor = generateRandomColor();
+
+  two.style.backgroundColor = randomColor;
+}
+
+one.addEventListener(`click`, handleClick);
+two.addEventListener(`mousemove`, onMousemove);
+
