21ae55116b905fa55e846125b489c307503d0986
me-neeraj135 <me.neeraj1455@gmail.com>
2022-03-13 12:05:41 +0530

    todoList

--- block-BJaaev/code/script.js
+++ block-BJaaev/code/script.js
@@ -4,16 +4,11 @@ let mainBox = document.createElement(`div`);
 mainBox.classList.add(`container`);
 let h1 = document.createElement(`h1`);
 h1.innerText = `todoS`;
-let root = document.createElement(`ul`);
+let ulRoot = document.createElement(`ul`);
 // append h1 into mainBox
 mainBox.append(h1);
 let div = document.createElement(`div`);
 div.classList.add(`inputTextDiv`);
-
-// creat scroll-down button and input text field
-let showAll = document.createElement(`label`);
-showAll.innerText = `>`;
-showAll.classList.add(`showAll`);
 
 //  input todo field
 
@@ -21,172 +16,164 @@ let textInput = document.createElement(`input`);
 textInput.type = `text`;
 textInput.placeholder = `What needs to be done?`;
 textInput.classList.add(`new-todo`);
-div.append(showAll, textInput);
-mainBox.append(div, root);
+div.append(textInput);
+mainBox.append(div, ulRoot);
 body.append(mainBox);
 
-// creat footer
+// creat footer....
 
 let footer = document.createElement(`footer`);
 let footerUl = document.createElement(`ul`);
 footerUl.classList.add(`footerUl`);
 
-// ************ item left button
-
-let itemLeft = document.createElement(`li`);
-itemLeft.innerText = `Item left`;
-let liTw = document.createElement(`li`);
-let allBtn = document.createElement(`button`);
-let liA2 = document.createElement(`a`);
-liA2.innerText = `All`;
-allBtn.append(liA2);
-liTw.append(allBtn);
-
-// Active button********
-
-let liTh = document.createElement(`li`);
-let activeBtn = document.createElement(`button`);
-let liA3 = document.createElement(`a`);
-activeBtn.append(liA3);
-liTh.append(activeBtn);
-liA3.innerText = `Active`;
-
-//Completed Button......
-
-let liFo = document.createElement(`li`);
-let completeBtn = document.createElement(`button`);
-let liA4 = document.createElement(`a`);
-liA4.innerText = `Completed`;
-completeBtn.append(liA4);
-liFo.append(completeBtn);
-
-// ********************Clear Completed Button
-
-let liFi = document.createElement(`li`);
-let completeClearBtn = document.createElement(`button`);
-let liA5 = document.createElement(`a`);
-liA5.innerText = `Clear completed`;
-
-completeClearBtn.append(liA5);
-liFi.append(completeClearBtn);
-footerUl.append(itemLeft, liTw, liTh, liFo, liFi);
-mainBox.append(footerUl);
-
-// creat an array to store all data as an object form
+//  store data in an array of object
 
 let allData = JSON.parse(localStorage.getItem(`todoS`)) || [];
 
-// creat handleInput function *********
+//item left button.....
 
-function handleInputText(event) {
-  let inputValue = event.target.value;
-  if (event.keyCode === 13 && event.target.value !== ``) {
-    let data = {};
-    data.name = inputValue;
-    data.isDone = false;
-    allData.push(data);
-    localStorage.setItem(`todoS`, JSON.stringify(allData));
-    creatUi(allData);
-    event.target.value = ``;
-  }
-}
+let itemLeft = document.createElement(`span`);
+itemLeft.innerText = `${allData.length} Left Item`;
+footerUl.append(itemLeft);
 
-// creat todo-delete function
+// all Button...
 
-function handleDelete(e) {
-  let id = e.target.dataset.id;
-  allData.splice(id, 1);
-  localStorage.setItem(`todoS`, JSON.stringify(allData));
-  itemLeft.innerText = `${allData.length}  Item left`;
-  creatUi(allData);
-}
+let all = document.createElement(`button`);
+all.classList.add(`all`);
+all.innerText = `All`;
+footerUl.append(all);
 
-//  creat handle toggle function
+// Active button......
 
-function handleToggle(e) {
-  let id = e.target.dataset.id;
-  allData[id].isDone = !allData[id].isDone;
-  localStorage.setItem(`todoS`, JSON.stringify(allData));
-  creatUi(allData);
-}
+let active = document.createElement(`button`);
+active.classList.add(`active`);
+active.innerText = `Active`;
+footerUl.append(active);
 
-// display all todo
+//Completed Button......
 
-function displayAllTodo(e) {
-  creatUi(allData);
-}
+let complete = document.createElement(`button`);
+complete.classList.add(`complete`);
+complete.innerText = `Completed`;
+footerUl.append(complete);
 
-// find active todo
+//Clear Completed Button.....
 
-function handleActive(event) {
-  let activeTodo = allData.filter(elm => elm.isDone === false);
-  creatUi(activeTodo);
-  console.log(activeTodo);
-}
+let clearCompleted = document.createElement(`button`);
+clearCompleted.classList.add(`clear-completed`);
+clearCompleted.innerText = `Clear Completed`;
+footerUl.append(clearCompleted);
 
-//  find completed todo
+mainBox.append(footerUl);
 
-function completedTodo(event) {
-  let completeTodo = allData.filter(elm => elm.isDone === true);
-  creatUi(completeTodo);
-  console.log(completeTodo);
-}
+// creat ui
 
-//  creat function to clear completed todo
-
-function clearCompleted(event) {
-  let clearComplete = allData.filter(elm => elm.isDone === true);
-  localStorage.setItem(`todoS`, JSON.stringify(clearComplete));
-  console.log(clearComplete);
-}
-
-// creat ui function
-
-function creatUi(dataInfo) {
-  root.innerHTML = ``;
+function creatUi(data = allData) {
+  ulRoot.innerHTML = ``;
   allData.forEach((elm, index) => {
     let li = document.createElement(`li`);
-    let checkInput = document.createElement(`input`);
-    checkInput.classList.add(`checkBox`);
-    checkInput.type = `checkbox`;
-    checkInput.setAttribute(`data-id`, index);
-    checkInput.checked = elm.isDone;
+    let checkBox = document.createElement(`input`);
+    checkBox.type = `checkbox`;
+    checkBox.setAttribute(`data-id`, index);
+    checkBox.classList.add(`checkBox`);
 
-    checkInput.addEventListener(`input`, handleToggle);
+    checkBox.addEventListener(`input`, e => {
+      let id = e.target.dataset.id;
+      allData[id].isDone = !allData[id].isDone;
+      creatUi();
+    });
 
     let p = document.createElement(`p`);
+    p.innerText = elm.text;
+    p.setAttribute(`data-id`, index);
 
-    p.innerText = elm.name;
-
-    if (checkInput.checked === true) {
+    if (elm.isDone === true) {
       p.classList.add(`lineThrough`);
     }
 
     let closeBtn = document.createElement(`button`);
-    closeBtn.innerText = `X`;
     closeBtn.classList.add(`close`);
+    closeBtn.innerText = `X`;
     closeBtn.setAttribute(`data-id`, index);
 
-    closeBtn.addEventListener(`click`, handleDelete);
+    closeBtn.addEventListener(`click`, handleClose);
+    itemLeft.innerText = `${allData.length} Left Item`;
 
-    li.append(checkInput, p, closeBtn);
-    root.append(li);
-    itemLeft.innerText = `${allData.length} Item left`;
+    li.append(checkBox, p, closeBtn);
+    ulRoot.append(li);
   });
 }
 
-textInput.addEventListener(`keyup`, handleInputText);
-activeBtn.addEventListener(`click`, handleActive);
-completeBtn.addEventListener(`click`, completedTodo);
-completeClearBtn.addEventListener(`click`, clearCompleted);
-allBtn.addEventListener(`click`, displayAllTodo);
+// Input todo
 
-// liBtn3.addEventListener(`click`, handleActive);
+function handleInputValue(event) {
+  inputValue = event.target.value;
+  if (event.keyCode === 13 && event.target.value !== ``) {
+    console.log(event.keyCode);
+    let data = {
+      text: inputValue,
+      isDone: false,
+    };
+    allData.push(data);
 
-//  <ul>
-//    <li style="display: flex">
-//      <input type="checkbox" />
-//      <p>add todo</p>
-//      <button>X</button>
-//    </li>
-//  </ul>;
+    creatUi();
+    // console.log(allData);
+
+    event.target.value = ``;
+    localStorage.setItem(`todoS`, JSON.stringify(allData));
+  }
+}
+//  close todo
+function handleClose(e) {
+  let id = e.target.dataset.id;
+  allData.splice(id, 1);
+  localStorage.setItem(`todoS`, JSON.stringify(allData));
+  creatUi();
+  itemLeft.innerText = `${allData.length} Left Item`;
+}
+
+// Display all todo
+
+function displayAll(e) {
+  creatUi(allData);
+  console.log(allData);
+}
+
+//  display only Active todo
+
+function handleActive(e) {
+  allData = allData.filter(elm => elm.isDone === false);
+  localStorage.setItem(`todoS`, JSON.stringify(allData));
+
+  creatUi();
+  itemLeft.innerText = `${activeTodo.length} Item Left`;
+  console.log(allData);
+}
+
+// display completed todo
+
+function handleCompleted(event) {
+  allData = allData.filter(element => element.isDone === true);
+
+  localStorage.setItem(`todoS`, JSON.stringify(allData));
+  creatUi();
+
+  console.log(allData);
+}
+
+// clear completed todo
+
+function handleClear(e) {
+  allData = allData.filter(elm => !elm.isDone);
+  console.log(allData);
+
+  localStorage.setItem(`todoS`, JSON.stringify(allData));
+  creatUi();
+  itemLeft.innerText = `${allData.length} Item Left`;
+}
+
+textInput.addEventListener(`keyup`, handleInputValue);
+all.addEventListener(`click`, displayAll);
+active.addEventListener(`click`, handleActive);
+complete.addEventListener(`click`, handleCompleted);
+clearCompleted.addEventListener(`click`, handleClear);
